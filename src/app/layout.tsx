877376061efa01a4960ed878782e0e7472a9a6e2
GreenKeewi <aliononmars07@gmail.com>
2025-09-12 16:08:52 -0400

    [dyad] Switched dark mode to use data-theme attribute and updated CSS variables - wrote 2 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
-          attribute="class"
+          attribute="data-theme" // Changed from "class" to "data-theme"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange

085bde7e3b19d4a42b28a69f672ec3130795e65a
GreenKeewi <aliononmars07@gmail.com>
2025-09-14 16:22:44 -0400

    Reverted all changes back to version 33f01b68e06d35d49016bc5d5478cc93a75c9d91

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Existing rule for component tagger in development
+  webpack: (config) => {
     if (process.env.NODE_ENV === "development") {
       config.module.rules.push({
         test: /\.(jsx|tsx)$/,
@@ -11,22 +10,6 @@ const nextConfig: NextConfig = {
         use: "@dyad-sh/nextjs-webpack-component-tagger",
       });
     }
-
-    // Remove any previous .wasm rules or copy-webpack-plugin configurations
-    config.module.rules = config.module.rules.filter(
-      (rule: any) => !(rule.test && rule.test.toString().includes('wasm'))
-    );
-    // Also remove CopyWebpackPlugin if it was added
-    config.plugins = config.plugins.filter(
-      (plugin: any) => plugin.constructor.name !== 'CopyWebpackPlugin'
-    );
-
-    // Add a standard rule to handle .wasm files as assets
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "asset/resource",
-    });
-
     return config;
   },
 };
